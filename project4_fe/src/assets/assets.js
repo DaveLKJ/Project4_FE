06d08d0980776ac4942046a23c040d2384c2a5b1
KongJian <kongjian3@hotmail.com>
2024-06-17 00:00:34 +0800

    add export to assets

--- project4_fe/src/assets/assets.js
+++ project4_fe/src/assets/assets.js
@@ -19,6 +19,9 @@ import bag_icon from "./bag_icon.png";
 import logout_icon from "./logout_icon.png";
 import parcel_icon from "./parcel_icon.png";
 
+import blended from "blended.png";
+import single_origin from "single_origin.png";
+
 import brazil_santos from "./brazil-santos.png";
 import ethiopian_apex_trio from "ethiopian_apex_trio.png";
 import ethiopian_guji_shakiso from "ethiopian_guji_shakiso.png";
@@ -26,5 +29,86 @@ import guatemala_geisha_el_mirador from "guatemala_geisha_el_mirador.png";
 import jamaica_blue_mountain from "jamaica_blue_mountain.png";
 import nanyang_kopi from "nanyang_kopi.png";
 
-import blended from "blended.png";
-import single_origin from "single_origin.png";
+export const assets = {
+  logo,
+  basket_icon,
+  header_img,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  facebook_icon,
+  twitter_icon,
+  cross_icon,
+  selector_icon,
+  profile_icon,
+  logout_icon,
+  bag_icon,
+  parcel_icon,
+};
+
+export const type_list = [
+  {
+    menu_name: "blended",
+    menu_image: blended,
+  },
+  {
+    menu_name: "single_origin",
+    menu_image: single_origin,
+  },
+];
+
+export const coffee_list = [
+  {
+    _id: "1",
+    name: "brazil santos",
+    image: brazil_santos,
+    price: 12,
+    description: "Tasteful notes to flavour your day",
+    category: "coffee bean",
+  },
+  {
+    _id: "2",
+    name: "ethiopian apex trio",
+    image: ethiopian_apex_trio,
+    price: 18,
+    description: "Tasteful notes to flavour your day",
+    category: "coffee bean",
+  },
+  {
+    _id: "3",
+    name: "ethiopian guji shakiso",
+    image: ethiopian_guji_shakiso,
+    price: 16,
+    description: "Tasteful notes to flavour your day",
+    category: "coffee bean",
+  },
+  {
+    _id: "4",
+    name: "guatemala geisha el mirador",
+    image: guatemala_geisha_el_mirador,
+    price: 24,
+    description: "Tasteful notes to flavour your day",
+    category: "coffee bean",
+  },
+  {
+    _id: "5",
+    name: "jamaica blue mountain",
+    image: jamaica_blue_mountain,
+    price: 14,
+    description: "Tasteful notes to flavour your day",
+    category: "coffee bean",
+  },
+  {
+    _id: "6",
+    name: "nanyang kopi",
+    image: nanyang_kopi,
+    price: 12,
+    description: "Tasteful notes to flavour your day",
+    category: "coffee bean",
+  },
+];
