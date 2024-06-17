4083370f79dfbbdc2d66b0f8dbf7c269ca3a7ad9
KongJian <kongjian3@hotmail.com>
2024-06-17 12:45:00 +0800

    add: coffeeitem useContext

--- project4_fe/src/pages/Home/Home.jsx
+++ project4_fe/src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import CoffeeDisplay from "../../components/CoffeeDisplay/CoffeeDisplay";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <CoffeeDisplay category={category} />
     </div>
   );
 };
